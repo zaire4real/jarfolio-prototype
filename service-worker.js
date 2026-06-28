a10825267d6f667d08ce8b5bffc368600d311140
zaire4real <123850787+zaire4real@users.noreply.github.com>
2026-06-28 23:14:02 +0800

    Add offline support
    Implement service worker for caching and offline support.

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const cacheName = "jarfolio-v0.1.0";
+const appShell = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(appShell))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => (
+      cached || fetch(event.request).catch(() => caches.match("./index.html"))
+    ))
+  );
+});
